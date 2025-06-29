eb92fd8b19073bd0da43b33c8863be411082f70b
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-06-29 10:31:37 +0000

    var must not used

--- 01_basics/01_varriable.js
+++ 01_basics/01_varriable.js
@@ -1,3 +1,12 @@
 const accId = 12345;
 let accEmail = "hariom@gmail.com";
 accCity = "jaipur";
+
+
+console.log(accId);
+
+// accId = 123456; // not allowed 
+
+
+// we should not use var type for the name of varriables 
+
