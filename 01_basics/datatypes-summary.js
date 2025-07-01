7a2fab89cf4e5d27222bf6b9c025e88690955df1
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-07-01 05:20:11 +0000

    takinng examples

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -37,3 +37,38 @@ const myFunction = function() {
     console.log("hello om");
     
 } 
+
+
+// *******************************************
+
+// stack (primitve), heap (non-primitive)
+
+let myName = "hariom singh"
+
+let anotherName = myName
+                           
+anotherName = "MoonMonger"; //maine ismein jab nayi value pass ki to actual value mein koi change nahi hua balki uski ek copy bani stack form meinn aur usmein change hua 
+
+console.log(myName);
+
+console.log(anotherName);
+
+
+// ye sab heap ke example practice kiyqa hai maine 
+// 
+
+
+let userOne ={
+    email: "hariom@gmail.com",
+    upi:  "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "moon@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
