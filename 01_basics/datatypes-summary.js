9ba04ceaf0f2a2b895765258439d7aeebeffa4a5
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-07-01 04:56:35 +0000

    data types sumaary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// primitive
+
+// 7types : string , number, boolean, null. undefined, symbol,BigInt
+
+const score = 100;
+const scorevalue = 100.3
+
+const isLogggedIn = false
+
+const outsideIn = null
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+const bigNumber = 123456789n
+
+
+// Reference (non primitive)
+
+// Array, Objects,Function
+
+
+const heros = ["shakti man", "nagraj", "doga"];
+
+
+let myObj = {
+
+    name:"hariom",
+    age: 22,
+
+}
+
+
+const myFunction = function() {
+    console.log("hello om");
+    
+} 
