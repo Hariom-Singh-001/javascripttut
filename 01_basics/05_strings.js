0c8da7b5fd5c1d24948b97c5e9fc29e5ebd03306
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-07-01 07:46:59 +0000

    learning strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,55 @@
+const name = "hariom"
+const repoCount = 3
+
+console.log(name + repoCount + "Value"); // is tarike se likhna is not good habbit 
+
+
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`); // ismein brackets ke andar single quote nahi laga hai DONT GET CONFUSE FROM IT**********
+
+// ANOTHER WAY TO DEFIB=NE STRINGS 
+const gameName = new String('hariom')
+
+console.log(gameName[0]);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('r'));
+
+
+const newString = gameName.substring(0,4);// it does not obey the negative value 
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);//iske andar negative value enter kar saktein hain 
+console.log(anotherString);
+
+// trim and replace 
+
+const newStringOne = "   hariom   "
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
