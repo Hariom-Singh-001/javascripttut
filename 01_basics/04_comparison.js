b167d92c3ad1598bae67f241755b6c978fc2414c
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-07-01 03:53:58 +0000

    learrning comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+console.log("2">1);
+
+console.log(null >= 0);//isne null ko zero mein convert kar diya phir compare kiya 
+
+
+console.log(undefined == 0);
+
+console.log(undefined >= 0);
+
+
+console.log(undefined > 0);
+
+
+console.log(undefined < 0);
+
