e444b8b454ed0abf0f456a6ec149d42f5a55dd1f
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-07-02 07:18:51 +0000

    studying properties of numbers

---
+++ 01_basics/06_nums-maths.js
@@ -0,0 +1,19 @@
+const score = 400;
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(1)); //to provide the precison after the decimal 
+
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-IN'));
+
+// ***********MATHS*****************
+
+
+
