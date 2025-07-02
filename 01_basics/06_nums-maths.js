94add6be26a6d77166bae7a66b0d416681a5ef66
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-07-02 08:07:38 +0000

    random property

--- 01_basics/06_nums-maths.js
+++ 01_basics/06_nums-maths.js
@@ -15,5 +15,36 @@ console.log(hundred.toLocaleString('en-IN'));
 
 // ***********MATHS*****************
 
+console.log(Math);
+console.log(Math.abs(-4)); // provide the absolute value
+
+console.log(Math.round(4.6));
+//two types ki hoti haiin 
+console.log(Math.ceil(4.2));//4 se thoda bhi jyada hoga to use 5 kardega and here ceil: ceiling means top values 
+
+console.log(Math.floor(4.2));//lowest value use karega 
+
+console.log(Math.min(4,5,2,8));
+
+
+console.log(Math.random());
+
+
+console.log((Math.random()*10) + 1);
+
+console.log((Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+Math.random
+
+
+
+
+
+
+
 
 
