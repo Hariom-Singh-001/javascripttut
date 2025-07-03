c2a268db564ca861774feff9e876f5412b14cad6
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-07-03 05:05:05 +0000

    exploring dates concept

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,26 @@
+// Dates 
+
+let myDate = new Date()
+
+console.log(myDate);
+
+
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleString());
+
+console.log(myDate.toISOString());
+
+console.log(myDate.toJSON());
+
+console.log(myDate.toUTCString());
+
+
+
+
+
+
+
+
