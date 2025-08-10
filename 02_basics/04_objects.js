1111cc3b0f06e2d0d6e9969d355e740ef53471b3
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-08-10 12:23:50 +0000

    making objects using constructor

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,29 @@
+// const tinderUser = new Object()
+
+const tinderUser ={}
+
+tinderUser.id = "123abc"
+tinderUser.name = "sumit"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hariom",
+            lastname: "singh"
+        }
+    }
+}
+
+
+// console.log(regularUser.fullname.userfullname);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+const onj3 = {}
