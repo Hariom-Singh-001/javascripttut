859597db1c3e91a9030559e881b3321a684583a1
Hariom-Singh-001 <singhhariom9757@gmail.com>
2025-08-11 23:58:25 +0000

    learning more about objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,4 +26,40 @@ const regularUser = {
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 
-const onj3 = {}
+// const obj3 = {obj1,obj2}
+
+const obj3 = {...obj1, ...obj2} //mostly used
+
+// const obj3 = Object.assign({}, obj1,obj2)
+
+
+console.log(obj3);
+
+
+const users = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+
+users[1].email
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));//this will make the array of the key value pair
+
+
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) // to check the avilability of the argument 
+
+
